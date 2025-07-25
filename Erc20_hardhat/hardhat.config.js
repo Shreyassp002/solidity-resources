9d6086107757cb79dcccf17916c9b18b8dba0537
Shreyas Patil <shreyassp002@gmail.com>
2025-07-25 20:18:08 +0530

    Initial Commit

---
+++ Erc20_hardhat/hardhat.config.js
@@ -0,0 +1,61 @@
+require("@nomicfoundation/hardhat-chai-matchers")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.6" },
+            { version: "0.8.20" },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localHost: {
+            url: "http://127.0.0.1:8545/",
+            //accounts: hardhat handles this
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    sourcify: {
+        // Disabled by default
+        // Doesn't need an API key
+        enabled: true,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        // coinmarketcap: COINMARKET_API_KEY
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    mocha: {
+        timeout: 300000,
+    },
+}
