2bede53ce16f8f5acbb5e106cd9bbde66ece1d7f
Shreyas Patil <shreyassp002@gmail.com>
2025-07-27 19:27:04 +0530

    Implemented aave lending pool adddress provider

--- Defi_hardhat/scripts/aaveBorrow.js
+++ Defi_hardhat/scripts/aaveBorrow.js
@@ -1,8 +1,32 @@
+const { getNamedAccounts } = require("hardhat")
 const { getWeth } = require("../scripts/getWeth")
 
 async function main() {
     //the protocol treats everthings as an ERC20 token
     await getWeth()
+    // const { deployer } = await getNamedAccounts
+
+    //lending pool provider address --> 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
+    const poolAddressesProviderAddress = "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e" // Mainnet V3
+
+    // 2. GET POOL ADDRESSES PROVIDER CONTRACT (V3) - Using installed package
+    const poolAddressesProvider = await ethers.getContractAt(
+        "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol:IPoolAddressesProvider",
+        poolAddressesProviderAddress,
+    )
+
+    console.log("PoolAddressesProvider:", poolAddressesProvider.target)
+
+    // 3. GET POOL ADDRESS FROM PROVIDER
+    const poolAddress = await poolAddressesProvider.getPool()
+    console.log("Pool address:", poolAddress)
+
+    // 4. GET POOL CONTRACT - Using installed package
+    const pool = await ethers.getContractAt(
+        "@aave/core-v3/contracts/interfaces/IPool.sol:IPool",
+        poolAddress,
+    )
+    console.log("Pool contract connected!")
 }
 
 main()
--- Defi_hardhat/scripts/getWeth.js
+++ Defi_hardhat/scripts/getWeth.js
@@ -1,7 +1,8 @@
-const { getNamedAccounts, deployments, ethers } = require("hardhat")
+const { getNamedAccounts, ethers } = require("hardhat")
 
 async function getWeth() {
-    //0x7b79995e5f793a07bc00c21412e50ecae098e7f9 aaddress weth
+    //0x7b79995e5f793a07bc00c21412e50ecae098e7f9 Sepolia weth address
+    //0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 Mainnet weth address
     const { deployer } = await getNamedAccounts()
     const iweth = await ethers.getContractAt("IWETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
     console.log(iweth.target)
@@ -9,7 +10,7 @@ async function getWeth() {
     const deployerSigner = await ethers.getSigner(deployer) // Turn address into a signer
     const tx = await iweth.connect(deployerSigner).deposit({ value: ethers.parseEther("1") })
     await tx.wait(1)
-    
+
     const wethBalance = await iweth.balanceOf(deployer)
     console.log(`WETH Balance: ${ethers.formatEther(wethBalance)} WETH`)
 }
