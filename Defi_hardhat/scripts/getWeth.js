22edc74256a5674d532e148fb9ef698f7a5bbfdf
Shreyas Patil <shreyassp002@gmail.com>
2025-07-28 15:11:47 +0530

    exported deposit amount

--- Defi_hardhat/scripts/getWeth.js
+++ Defi_hardhat/scripts/getWeth.js
@@ -1,5 +1,7 @@
 const { getNamedAccounts, ethers } = require("hardhat")
 
+const Deposit_Amount = ethers.parseEther("1")
+
 async function getWeth() {
     //0x7b79995e5f793a07bc00c21412e50ecae098e7f9 Sepolia weth address
     //0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 Mainnet weth address
@@ -8,11 +10,11 @@ async function getWeth() {
     console.log(iweth.target)
 
     const deployerSigner = await ethers.getSigner(deployer) // Turn address into a signer
-    const tx = await iweth.connect(deployerSigner).deposit({ value: ethers.parseEther("1") })
+    const tx = await iweth.connect(deployerSigner).deposit({ value: Deposit_Amount })
     await tx.wait(1)
 
     const wethBalance = await iweth.balanceOf(deployer)
     console.log(`WETH Balance: ${ethers.formatEther(wethBalance)} WETH`)
 }
 
-module.exports = { getWeth }
+module.exports = { getWeth, Deposit_Amount }
